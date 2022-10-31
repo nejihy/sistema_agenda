597dfec11bd3f4834a64e9e01dabeb62cc8345ea
Robson Pavanelli <robson@buddemeyer.com.br>
2022-10-31 13:44:50 -0300

    Alterando a função de excluir eventos com hooks.

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,9 @@ function App() {
     // }
     // setEventos([...eventos])
   }
-  const deletarEvento = (id: number) => {
-    // setEventos([...eventos.filter(evento => evento.id !== id)])
-  }
+  //const deletarEvento = (id: number) => {
+  // setEventos([...eventos.filter(evento => evento.id !== id)])
+  //}
 
   const aplicarFiltro = (data: Date | null) => {
     setFiltro(data)
@@ -69,7 +69,6 @@ function App() {
             <ListaDeEventos
               aoFiltroAplicado={aplicarFiltro}
               aoAlterarStatus={alterarStatusEvento}
-              aoDeletarEvento={deletarEvento}
             />
           </Card>
         </div>
